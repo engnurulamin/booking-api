5b6c972282c94801f0be3b285c4def283851267a
Nurul Amin <namin2750@gmail.com>
2023-11-15 22:00:53 +0600

    create hotel

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ mongoose.connection.on("disconnected", () => {
   console.log("mongoDB disconnected");
 });
 
+app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
--- src/routes/hotels.js
+++ src/routes/hotels.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const Hotel = require("../models/Hotel");
 
 const router = express.Router();
+router.post("/", async (req, res) => {
+  const newHotel = new Hotel(req.body);
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
