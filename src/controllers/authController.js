86ff1fc601985730f79ffb1af65616c2691c528f
Nurul Amin <namin2750@gmail.com>
2023-11-16 21:15:52 +0600

    set cookeies

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const { createError } = require("../utils/error");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res, next) => {
   try {
@@ -32,8 +33,17 @@ const login = async (req, res, next) => {
     if (!isCorrectPassword) {
       return next(createError(400, "Wrong password or username"));
     }
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT_KEY
+    );
     const { password, isAdmin, ...others } = user._doc;
-    res.status(200).json({ ...others });
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json({ ...others });
   } catch (error) {
     next(error);
   }
