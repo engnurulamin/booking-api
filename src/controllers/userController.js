e16e01261ea4572620ab5f3d8bdbe00648bc5f1a
Nurul Amin <namin2750@gmail.com>
2023-11-16 22:11:10 +0600

    user CRUD

---
+++ src/controllers/userController.js
@@ -0,0 +1,48 @@
+const User = require("../models/User");
+
+const getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    next(err);
+  }
+};
+
+const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+const updateUser = async (req, res, next) => {
+  try {
+    const updatededUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatededUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+const deleteUser = async (req, res, next) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  deleteUser,
+  updateUser,
+  getUser,
+};
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,23 @@
 const express = require("express");
+const {
+  updateUser,
+  deleteUser,
+  getUser,
+  getAllUsers,
+} = require("../controllers/userController");
 
 const router = express.Router();
+
+// Update
+router.put("/:id", updateUser);
+
+// Delete
+router.delete("/:id", deleteUser);
+
+// Get One
+router.get("/:id", getUser);
+
+// Get All
+router.get("/", getAllUsers);
 
 module.exports = router;
