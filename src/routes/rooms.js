4bab37a56c90d8209ed638e5a6e409980ec266c1
Nurul Amin <namin2750@gmail.com>
2023-11-17 20:11:56 +0600

    room route

--- src/routes/rooms.js
+++ src/routes/rooms.js
@@ -1,5 +1,29 @@
 const express = require("express");
 
+const { verifyAdmin } = require("../utils/verifyToken");
+const {
+  createRoom,
+  updateRoom,
+  deleteRoom,
+  getRoom,
+  getAllRooms,
+} = require("../controllers/roomController");
+
 const router = express.Router();
+
+// Create
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+// Update
+router.put("/:id", verifyAdmin, updateRoom);
+
+// Delete
+router.delete("/:id", verifyAdmin, deleteRoom);
+
+// Get One
+router.get("/:id", getRoom);
+
+// Get All
+router.get("/", getAllRooms);
 
 module.exports = router;
