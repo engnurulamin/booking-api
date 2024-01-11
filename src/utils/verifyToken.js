970f31f8662e7cb99ab8664f1a70fb77eea4b2e3
Nurul Amin <namin2750@gmail.com>
2024-01-11 21:40:15 +0600

    fix

--- src/utils/verifyToken.js
+++ src/utils/verifyToken.js
@@ -4,11 +4,11 @@ const { createError } = require("./error");
 const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) {
-    return next(createError(401, "You are not authenticated!"));
+    next(createError(401, "You are not authenticated!"));
   }
 
   jwt.verify(token, process.env.JWT_KEY, (err, user) => {
-    if (err) return next(createError(403, "Token is not valid!"));
+    if (err) next(createError(403, "Token is not valid!"));
     req.user = user;
     next();
   });
@@ -19,17 +19,17 @@ const verifyUser = (req, res, next) => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
       next();
     } else {
-      return next(createError(403, "You are not authorized!"));
+      next(createError(403, "You are not authorized!"));
     }
   });
 };
 
 const verifyAdmin = (req, res, next) => {
-  verifyToken(req, res, next, () => {
-    if (req.user.isAdmin) {
+  verifyToken(req, res, () => {
+    if (req.user && req.user.isAdmin) {
       next();
     } else {
-      return next(createError(403, "You are not an Admin!"));
+      next(createError(403, "You are not an Admin!"));
     }
   });
 };
